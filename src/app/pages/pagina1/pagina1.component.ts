93b7dc77450e8ad21ccc876c64ed9089f3413998
Inma Gijón <igijoninf@cifpvirgendegracia.com>
2022-07-04 11:51:40 +0200

    ngOnDestroy

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
@@ -11,7 +12,9 @@ export class Pagina1Component
                   AfterViewInit, AfterViewChecked, OnDestroy {
 
   nombre: string = 'Inma';
-  
+  segundos: number = 0;
+  timerSubscription!: Subscription;
+
   constructor() {
     console.log('constructor');
 
@@ -36,10 +39,14 @@ export class Pagina1Component
   }
   ngOnDestroy(): void {
     console.log('ngOnDestroy');
+    this.timerSubscription.unsubscribe();
   }
 
   ngOnInit(): void {
     console.log('ngOnInit');
+    this.timerSubscription = interval(1000).subscribe( i => {
+      this.segundos = i
+    });
   }
 
   guardar(): void {
